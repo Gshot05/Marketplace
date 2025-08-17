9bb2201fd0cca88657e8b6ef2126c81bb71d2767
Islam <islam.sh.r@icloud.com>
2025-08-17 23:25:39 +0300

    tam tut

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -12,6 +12,8 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine, pool *pgxpool.Pool) {
+
+	// CORS
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:8080"},
 		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
@@ -21,32 +23,36 @@ func RegisterRoutes(r *gin.Engine, pool *pgxpool.Pool) {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	authRepo := repository.NewAuthRepo(pool)
-	offerRepo := repository.NewOfferRepository(pool)
-	serviceRepo := repository.NewServiceRepository(pool)
-	favoriteRepo := repository.NewFavoriteRepository(pool)
-
-	authHandler := handlers.NewAuthHandler(authRepo)
-	offerHandler := handlers.NewOfferHandler(offerRepo)
-	serviceHandler := handlers.NewServiceHandler(serviceRepo)
-	favoriteHandler := handlers.NewFavoriteHandler(favoriteRepo)
-
+	// Auth
 	auth := r.Group("/auth")
+	authRepo := repository.NewAuthRepo(pool)
+	authHandler := handlers.NewAuthHandler(authRepo)
 	auth.POST("/register", authHandler.Register())
 	auth.POST("/login", authHandler.Login())
 
+	// Handlers group
 	v1 := r.Group("/api")
 	v1.Use(middleware.AuthMiddleware)
+
+	// Offers
+	offerRepo := repository.NewOfferRepository(pool)
+	offerHandler := handlers.NewOfferHandler(offerRepo)
 	v1.POST("/offers", offerHandler.CreateOffer())
 	v1.PATCH("/offers", offerHandler.UpdateOffer())
 	v1.DELETE("/offers", offerHandler.DeleteOffer())
 	v1.GET("/offers", offerHandler.ListOffers())
 
+	// Services
+	serviceRepo := repository.NewServiceRepository(pool)
+	serviceHandler := handlers.NewServiceHandler(serviceRepo)
 	v1.POST("/services", serviceHandler.CreateService())
 	v1.PATCH("/services", serviceHandler.UpdateService())
 	v1.DELETE("/services", serviceHandler.DeleteService())
 	v1.GET("/services", serviceHandler.ListServices())
 
+	// Favorites
+	favoriteRepo := repository.NewFavoriteRepository(pool)
+	favoriteHandler := handlers.NewFavoriteHandler(favoriteRepo)
 	v1.POST("/favorites", favoriteHandler.AddFavorite())
 	v1.DELETE("/favorites", favoriteHandler.DeleteFavorite())
 	v1.GET("/favorites", favoriteHandler.ListFavorites())
