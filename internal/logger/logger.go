0aaa057689c38a1805a981ee3e7be20c76595707
Islam <islam.sh.r@icloud.com>
2025-08-20 19:40:13 +0300

    async update for logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -16,36 +17,35 @@ func NewLogger(pool *pgxpool.Pool) *Logger {
 	return &Logger{pool: pool}
 }
 
-func (l *Logger) Log(level string, format string, args ...interface{}) error {
-	message := fmt.Sprintf(format, args...)
-
-	_, err := l.pool.Exec(
-		context.Background(),
-		"INSERT INTO logs (level, message, created_at) VALUES ($1, $2, $3)",
-		level,
-		message,
-		time.Now(),
-	)
-
-	if err != nil {
-		return fmt.Errorf("logger failed: %w", err)
-	}
-	return nil
+func (l *Logger) LogAsync(level string, format string, args ...interface{}) {
+	go func() {
+		message := fmt.Sprintf(format, args...)
+		_, err := l.pool.Exec(
+			context.Background(),
+			"INSERT INTO logs (level, message, created_at) VALUES ($1, $2, $3)",
+			level,
+			message,
+			time.Now(),
+		)
+		if err != nil {
+			log.Printf("Async log failed: %v", err)
+		}
+	}()
 }
 
 // Удобные методы-обертки
 func (l *Logger) Info(format string, args ...interface{}) {
-	_ = l.Log("INFO", format, args...)
+	l.LogAsync("INFO", format, args...)
 }
 
 func (l *Logger) Error(format string, args ...interface{}) {
-	_ = l.Log("ERROR", format, args...)
+	l.LogAsync("ERROR", format, args...)
 }
 
 func (l *Logger) Debug(format string, args ...interface{}) {
-	_ = l.Log("DEBUG", format, args...)
+	l.LogAsync("DEBUG", format, args...)
 }
 
 func (l *Logger) Warn(format string, args ...interface{}) {
-	_ = l.Log("WARN", format, args...)
+	l.LogAsync("WARN", format, args...)
 }
